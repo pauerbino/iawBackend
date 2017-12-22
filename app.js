ea0dae10c68bf0ead6459d53fefca10629af8451
pujoljoaquin <pujoljoaquin@hotmail.com>
2017-12-22 16:38:35 -0300

    fix cors permisos

--- app.js
+++ app.js
@@ -27,11 +27,7 @@ var register = require('./routes/register');
 var app = express();
 app.use(cors());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next()
-})
+app.use(cors({origin: 'https://iawbackend.herokuapp.com/'}));
 
 app.use(passport.initialize());
 // app.use(logger('dev'));
