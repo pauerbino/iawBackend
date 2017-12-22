968ab5c0abf9c5ee6a6c08f76d867acf6436be8c
pujoljoaquin <pujoljoaquin@hotmail.com>
2017-12-22 16:11:12 -0300

    mongo connect heroku

--- app.js
+++ app.js
@@ -11,7 +11,11 @@ const cors = require('cors');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/marketingDB', {useMongoClient: true})
+//mongoose.connect('mongodb://localhost:27017/marketingDB', {useMongoClient: true})
+//  .then(() =>  console.log('connection successful'))
+//  .catch((err) => console.error(err));
+  
+mongoose.connect(process.env.MONGOLAB_URI || process.env.MONGODB_URI || 'mongodb://localhost/marketingDB', {useMongoClient: true})
   .then(() =>  console.log('connection successful'))
   .catch((err) => console.error(err));
 
