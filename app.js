233e90c15d9a4a8b0a7a1dfddcaf9556b0d06ba5
pujoljoaquin <pujoljoaquin@hotmail.com>
2017-12-22 16:46:27 -0300

    cors fix

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var passport = require('passport');
 
-//const cors = require('cors');
+const cors = require('cors');
 
 
 mongoose.Promise = global.Promise;
@@ -25,7 +25,12 @@ var campaigns = require('./routes/campaigns');
 var users = require('./routes/users');
 var register = require('./routes/register');
 var app = express();
-//app.use(cors());
+app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(passport.initialize());
 // app.use(logger('dev'));
